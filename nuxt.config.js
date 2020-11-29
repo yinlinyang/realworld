1e2dfb72014db15a8fede56482d26ec388c98a3a
尹钦 <yinqin@yinqindeMacBook-Pro.local>
2020-11-30 07:59:19 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  router: {
+    extendRoutes (routes, resolve) {
+      routes.splice(0)
+      routes.push(...[
+        {
+          path: '/',
+          component: resolve(__dirname, 'pages/layout')
+        }
+      ])
+    }
+  }
+}
