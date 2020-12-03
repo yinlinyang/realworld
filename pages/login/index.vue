a5dfdd832e5c3ca20003e918bf0a53f3ad5d1767
yinqindx <yinqin@dxchain.onaliyun.com>
2020-12-03 09:29:16 +0800

    feat: 登入状态持久化

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,84 +1,91 @@
   <template>
-	<div class="auth-page">
-		<div class="container page">
-			<div class="row">
-				<div class="col-md-6 offset-md-3 col-xs-12">
-					<h1 class="text-xs-center">Sign up</h1>
-					<p class="text-xs-center">
-						<nuxt-link to="/login">Have an account?</nuxt-link>
-					</p>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign up</h1>
+          <p class="text-xs-center">
+            <nuxt-link to="/login">Have an account?</nuxt-link>
+          </p>
 
-					<ul class="error-messages">
-						<li>That email is already taken</li>
-					</ul>
+          <ul class="error-messages">
+            <template v-for="(message, field) in errors" >
+              <li :key="field">{{ field }} {{ message }}</li>
+            </template>
+          </ul>
 
-					<form @submit.prevent="onSubmit">
-						<fieldset class="form-group" v-if="!isLogin">
-							<input
-								class="form-control form-control-lg"
-								type="text"
-								placeholder="Your Name"
-								v-model="user.username"
-							/>
-						</fieldset>
-						<fieldset class="form-group">
-							<input
-								class="form-control form-control-lg"
-								type="text"
-								placeholder="Email"
-								v-model="user.email"
-							/>
-						</fieldset>
-						<fieldset class="form-group">
-							<input
-								class="form-control form-control-lg"
-								type="password"
-								placeholder="Password"
-								v-model="user.password"
-							/>
-						</fieldset>
-						<button class="btn btn-lg btn-primary pull-xs-right">
-							Sign up
-						</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group" v-if="!isLogin">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Your Name"
+                v-model="user.username"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+                v-model="user.email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+                v-model="user.password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
   
 <script>
 import { login, register } from '@/api/user'
 import { mapMutations } from 'vuex'
+
+const Cookie = process.client ? require('js-cookie') : null
+
 export default {
-	name: "Login",
-	data () {
-		return {
-			user: {
-				username: '',
-				email: '',
-				password: '12345678',
-			}
-		}
-	},
-	computed: {
-		isLogin () {
-			return this.$route.name === 'login'
-		}
-	},
-	methods: {
-		...mapMutations(['setUser']),
-		async onSubmit () {
-			try {
-				const user = this.user
-				const { data } = this.isLogin ? await login({user}) : await register({user})
-				this.$router.push('/')
-				this.setUser(data.user)
-			} catch (error) {
-				console.log(error)
-			}
-		}
-	}
+  name: "Login",
+  data () {
+    return {
+      user: {
+        username: '',
+        email: '',
+        password: '12345678',
+      },
+      errors: []
+    }
+  },
+  computed: {
+    isLogin () {
+      return this.$route.name === 'login'
+    }
+  },
+  methods: {
+    ...mapMutations(['setUser']),
+    async onSubmit () {
+      try {
+        const user = this.user
+        const { data } = this.isLogin ? await login({ user }) : await register({ user })
+        this.$router.push('/')
+        Cookie.set('user', data.user)
+        this.setUser(data.user)
+      } catch (err) {
+        this.errors = err.response.data.errors
+      }
+    }
+  }
 }
 </script>
   
